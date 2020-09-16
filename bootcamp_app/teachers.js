0fb33e01ef3e52a81d7da915bd28ef71019d4ac8
Keith Ong <keith.ong5000@gmail.com>
2020-09-16 23:36:02 +0000

    SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
 SELECT teachers.name as name, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name
-`)
+LIMIT $2;
+`;
+
+pool.query(queryString,values)
 .then(res => {
   console.log(res.rows)
   res.rows.forEach(teacher => {
